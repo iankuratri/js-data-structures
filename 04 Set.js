e1f66f0aeff9e109800674e63b847caf6a9ffc1b
Ankur Atri <ankuratri.in@gmail.com>
2023-07-09 18:32:45 +0530

    sets methods added

--- 04 Set.js
+++ 04 Set.js
@@ -14,6 +14,16 @@ Sets do not maintain an insertion order.
 Sets are iterables. They can be used with a for of loop.
 
 
+Its main methods are:
+
+new Set([iterable]) – creates the set, and if an iterable object is provided (usually an array), copies values from it into the set.
+set.add(value) – adds a value, returns the set itself.
+set.delete(value) – removes the value, returns true if value existed at the moment of the call, otherwise false.
+set.has(value) – returns true if the value exists in the set, otherwise false.
+set.clear() – removes everything from the set.
+set.size – is the elements count.
+
+
 Set vs Array
 
 Arrays can contain duplicate values whereas Sets cannot.
