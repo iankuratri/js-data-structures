a80aa7cf4e3c61b1719701509a5e9c1d55e43980
Ankur Atri <ankuratri.in@gmail.com>
2023-07-13 09:19:13 +0530

    Linked List Node & Class added

--- 11 Linked List.js
+++ 11 Linked List.js
@@ -22,3 +22,31 @@ All applications of both stacks and queues are applications of linked lists
 Image viewer
 
 */
+
+// Linked List - Node
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+}
+
+const list = new LinkedList();
+
+console.log("Is list empty?", list.isEmpty());
+console.log("List size", list.getSize());
