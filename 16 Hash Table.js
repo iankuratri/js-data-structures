f9840e54bd877589bd2746702ab983ff2f5a3a72
Ankur Atri <ankuratri.in@gmail.com>
2023-07-25 08:49:28 +0530

    Hash Table Collisions handled

--- 16 Hash Table.js
+++ 16 Hash Table.js
@@ -62,17 +62,48 @@ class HashTable {
 
   set(key, value) {
     const index = this.hash(key);
-    this.table[index] = value;
+    const bucket = this.table[index];
+
+    if (!bucket) {
+      this.table[index] = [[key, value]];
+    } else {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+
+      if (sameKeyItem) {
+        sameKeyItem[1] = value;
+      } else {
+        bucket.push([key, value]);
+      }
+    }
   }
 
   get(key) {
     const index = this.hash(key);
-    return this.table[index];
+    const bucket = this.table[index];
+
+    if (bucket) {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+
+      if (sameKeyItem) {
+        return sameKeyItem[1];
+      }
+    }
+
+    return undefined;
   }
 
   remove(key) {
     const index = this.hash(key);
-    this.table[index] = undefined;
+
+    const bucket = this.table[index];
+
+    if (bucket) {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+
+      if (sameKeyItem) {
+        bucket.splice(bucket.indexOf(sameKeyItem), 1);
+      }
+    }
   }
 
   display() {
@@ -93,6 +124,10 @@ table.display();
 
 console.log(table.get("name"));
 
-table.remove("name");
+// table.remove("name");
+
+table.set("mane", "Clark");
+
+table.set("name", "Diana");
 
 table.display();
