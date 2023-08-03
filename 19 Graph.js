be37cb531691c43f447b6a50a387ef910a5793f7
Ankur Atri <ankuratri.in@gmail.com>
2023-08-03 09:30:46 +0530

    Graph overview added

---
+++ 19 Graph.js
@@ -0,0 +1,39 @@
+/**
+
+Graph
+
+A graph is a non-linear data structure that consists of a finite 
+number of vertices (also called nodes) connected by edges.
+
+Trees are a specific type of graph data structure.
+
+Types:
+- Directed
+- Undirected
+
+Directed Graph:
+A graph in which the edges have a direction.
+Edges are usually represented by arrows pointing in the direction the graph can be traversed.
+
+Undirected Graph:
+A graph in which the edges are bidirectional.
+The graph can be traversed in either direction.
+The absence of an arrow tells us that the graph is undirected.
+
+Usage:
+- Google maps
+- Social media sites
+
+Graph Representation:
+- Adjacency matrix
+- Adjacency list
+
+Adjacency Matrix:
+An adjacency matrix is a 2D array of size V × V where V is the number of vertices in the graph.
+
+Each row and column represent a vertex.
+
+If the value of any element say, matrix[i][j] is 1, it represents that 
+there is an edge connecting vertex i and vertex j
+
+*/
