19ddb8766b3d083dbbaa1f70e3a5f54736be4b58
Ankur Atri <ankuratri.in@gmail.com>
2023-08-03 09:43:49 +0530

    Info about Adjacency List of a graph added

--- 19 Graph.js
+++ 19 Graph.js
@@ -7,6 +7,7 @@ number of vertices (also called nodes) connected by edges.
 
 Trees are a specific type of graph data structure.
 
+
 Types:
 - Directed
 - Undirected
@@ -20,21 +21,37 @@ A graph in which the edges are bidirectional.
 The graph can be traversed in either direction.
 The absence of an arrow tells us that the graph is undirected.
 
+
 Usage:
 - Google maps
 - Social media sites
+
 
 Graph Representation:
 - Adjacency matrix
 - Adjacency list
 
+
 Adjacency Matrix:
 An adjacency matrix is a 2D array of size V × V where V is the number of vertices in the graph.
-
 Each row and column represent a vertex.
+If the value of any element say, matrix[i][j] is 1, it represents that there is an edge connecting vertex i and vertex j
 
-If the value of any element say, matrix[i][j] is 1, it represents that 
-there is an edge connecting vertex i and vertex j
+Adjacency List:
+Vertices are stored in a map like data structure, and every vertex stores a list of its adjacent vertices
+
+
+Adjacency Matrix vs Adjacency List:
+With an adjacency list, we only need to store the values for the edges that exist.
+
+With adjacency matrix, you store values irrespective of whether an edge exists or not. 
+Storage wise, an adjacency list is way more efficient.
+
+With adjacency list, inserting and finding adjacent nodes is constant time complexity 
+whereas with adjacency matrix, it is linear time complexity.
+
+An adjacency list allows you to store additional values with an edge such as weight of the edge. 
+With adjacency matrix, such information would have to be stored externally.
 
 */
 
@@ -48,3 +65,14 @@ const matrix = [
 console.log("A is connected to A?", matrix[0][0]);
 console.log("A is connected to B?", matrix[0][1]);
 console.log("A is connected to C?", matrix[0][2]);
+
+// Adjacency List
+const list = {
+  A: ["B"],
+  B: ["A", "C"],
+  C: ["B"],
+};
+
+console.log("A is connected to?", list["A"]);
+console.log("B is connected to?", list["B"]);
+console.log("C is connected to?", list["C"]);
