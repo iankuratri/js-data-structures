6e8feb1a3637749f2453b5dd86c2cab78359cd28
Ankur Atri <ankuratri.in@gmail.com>
2023-08-04 23:47:26 +0530

    Graph Add Vertex and Edge methods added

--- 19 Graph.js
+++ 19 Graph.js
@@ -76,3 +76,35 @@ const list = {
 console.log("A is connected to?", list["A"]);
 console.log("B is connected to?", list["B"]);
 console.log("C is connected to?", list["C"]);
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = new Set();
+    }
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1]) {
+      this.addVertex(vertex1);
+    }
+    if (!this.adjacencyList[vertex2]) {
+      this.addVertex(vertex2);
+    }
+    this.adjacencyList[vertex1].add(vertex2);
+    this.adjacencyList[vertex2].add(vertex1);
+  }
+}
+
+const graph = new Graph();
+
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+
+graph.addEdge("A", "B");
+graph.addEdge("B", "C");
