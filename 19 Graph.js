fda751d4a9415d73c084ba7e809d083e97688863
Ankur Atri <ankuratri.in@gmail.com>
2023-08-04 23:55:14 +0530

    display and has edge method added

--- 19 Graph.js
+++ 19 Graph.js
@@ -98,6 +98,19 @@ class Graph {
     this.adjacencyList[vertex1].add(vertex2);
     this.adjacencyList[vertex2].add(vertex1);
   }
+
+  hasEdge(vertex1, vertex2) {
+    return (
+      this.adjacencyList[vertex1].has(vertex2) &&
+      this.adjacencyList[vertex2].has(vertex1)
+    );
+  }
+
+  display() {
+    for (const vertex in this.adjacencyList) {
+      console.log(vertex + " -> " + [...this.adjacencyList[vertex]]);
+    }
+  }
 }
 
 const graph = new Graph();
@@ -108,3 +121,8 @@ graph.addVertex("C");
 
 graph.addEdge("A", "B");
 graph.addEdge("B", "C");
+
+graph.display();
+
+console.log("Edge between A and B?", graph.hasEdge("A", "B"));
+console.log("Edge between A and C?", graph.hasEdge("A", "C"));
